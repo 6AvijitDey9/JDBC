86e23b9ba52d655076932f62b302ae2b8eb6c0a9
Avijit Dey <110467299+6AvijitDey9@users.noreply.github.com>
2022-12-08 02:48:32 +0530

    Add files via upload

---
+++ Java DataBase Connectivity Basic/Helper.java
@@ -0,0 +1,36 @@
+package Com.Student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Helper {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static Connection con() throws SQLException{
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/avijit_dey", "root", "root");
+	}
+
+	public static void main(String[] args) throws SQLException {
+		MenuDriven md = new MenuDriven();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("(Press 1 for Save)(Press 2 for Fetch)(Press 3 for Update)(Press 4 for delete)");
+		int var = sc.nextInt();
+		if(var==1) {
+			md.saveMenuDriven();}
+		if(var==2) {
+			md.fetchMenuDriven();}
+		if(var==3) {
+			md.updateMenuDriven();}
+		if(var==4) {
+			md.deleteMenuDriven();}
+		}
+	}
+
